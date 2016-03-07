44b6e19c1febef0b153c0899289a040fffd6f82d
Bruno Arthur Cesconetto <brunoartc@yahoo.com>
2016-03-06 22:18:56 -0300

    Clock,bluetooth
    replace clock system and place bluetooth

--- bard.ino
+++ bard.ino
@@ -1,51 +1,182 @@
-// Iv=5 LCD=(11,10,9,8,7,6) Rele=12 Botao=4 #4,5,6,7,8,9,10,11
-// #1,2,3,12,13
-unsigned long milise=0; 
-#include <IRremote_library.h> //#include <IRremote>
-#include <LiquidCrystal.h>
-LiquidCrystal lcd(11,10,9,8,7,6);
-const int IV = 5;
-int temp,tempan,cont=0,seg=0,minut=0,hor=0,parado=0,minuta=0,hora=0,b=3000,timer=0,definido=5000,beep=50,desligar=0,timerr=90; 
-bool Status,Status2,alarme,beeps;
-IRrecv Rec(IV);
-decode_results results;
-void setup()
-{
-  lcd.begin(16,2);
-  lcd.setCursor(0,0);
-  lcd.print("                ");
-  Serial.begin(9600);
-  Rec.enableIRIn();
-  pinMode(12,1);
-  pinMode(4,0);
-  pinMode(13,1);
-  pinMode(3,1);
-}
 
+unsigned long milise=0;
+#include <IRremote.h>
+#include <LiquidCrystal.h>
+#include <Time.h>
+LiquidCrystal lcd(11,10,9,8,7,6);
+int temp,tempan,cont=0,seg=0,parado=0,minuta=0,hora=0,b=3000,timer=0,definido=120,beep=50,desligar=0,timerr=90,hor=0,minut=0,hor1=0,minut1=0; 
+char biit;
+bool Status,Status2,alarme,beeps,mudo=1;
+IRrecv Rec(5);
+decode_results results;
+void setup(){
+  setTime(0,0,0,1,1,2000); //Define clock | Define o tempo
+  lcd.begin(16,2); // LCD Dimensions | Dimensoes do LCD
+  lcd.setCursor(0,0);lcd.print("                "); //Clear LCD Screen | Limpa tela LCD
+  Serial.begin(9600); //Starts USB/Bluethoot comunicatio | Comeca coneccao UBS/Bluethoot
+  Rec.enableIRIn(); //Starts IR reciver | Comeca o receptor de IV
+  pinMode(12,1); // Relay|Rele
+  pinMode(4,0); //Button |Botao
+  pinMode(13,1); // LCD Bright | Brilho LCD
+  pinMode(3,1); // Buzzer
+    digitalWrite(3,1);
+    delay(50);
+    digitalWrite(3,0);
+    delay(50);
+    digitalWrite(3,1);
+    delay(50);
+    digitalWrite(3,0);
+    delay(50);
+    digitalWrite(3,1);
+    delay(100);
+    digitalWrite(3,0);
+}
+void MostraTempo(){
+  lcd.setCursor(0,1);lcd.print(hour());lcd.print(":");lcd.print(minute());lcd.print(':');lcd.print(second());lcd.print(" ");
+  lcd.print(temp);lcd.print("oC");
+  if (alarme) {lcd.print(" a");} else if (desligar>0){lcd.print(desligar);} else {lcd.print("    ");}
+}
 void loop() {
-//  digitalWrite (13,1); teste, deixar ligado LCD
-  if (timer>0){
+  //digitalWrite (13,1);/* teste, deixar ligado LCD */
+  MostraTempo();
+  while (timer>0){
     timer--;
-    if (beep>0){
-      if (beep==50){
-        if (beeps) {digitalWrite(3,0);Serial.println('a');beeps=0;}else {digitalWrite(3,1);Serial.println('b');beeps=1;}
-      }
-      beep--;  
-    }else beep=50;
+    if (digitalRead(4)==0) timer=0;
+    
+    digitalWrite(3,1);Serial.println('a');
+    digitalWrite(12,0);
+    Serial.println("DesLiga");
+    delay(300);
+    
+    digitalWrite(3,0);Serial.println('b');
+    Serial.println(timer);
+    digitalWrite(12,1);
+    Serial.println("Liga");
+    desligar=timerr;
+    delay(200);
+    Status=1; 
+    alarme=0;
   }
+
+  
   b++;
-  Serial.println(timer);
-  synchora();
   lcd.setCursor(0,0);
-  tempan=temp;
-  temp=analogRead(A0);
-  if (tempan-temp>5 || tempan-temp<-5) temp=map(temp,0,1023,0,510); //60=30 41=19
-//
-  lcd.setCursor(0,0);lcd.print("                ");
-  synchora();
- // Serial.print("Temp=");Serial.println(temp);
-  if (Rec.decode(&results)) {
-    Serial.println("infraVermelho");
+  //Serial.println(second());
+  //Serial.println(minute());
+  //Serial.println(timer);
+  tempan=temp;temp=analogRead(A0);
+  if (tempan-temp>5 || tempan-temp<-5 || b>100) temp=map(temp,0,1023,0,510); //60=30 41=19
+
+
+
+  if (Serial.available()){
+    Serial.println("Bluetooth:");
+    biit = Serial.read();
+    while (biit=='a'){
+      if (Serial.available()){
+        hor=Serial.read();
+        while (hor>-1){
+          if (Serial.available()){
+          hor1=Serial.read();
+            while (hor1>-1){
+              if (Serial.available()){
+                minut=Serial.read();
+                while (minut>-1){
+                  if (Serial.available()){
+                    minut1=Serial.read();
+                    hor1=((hor-48)*10)+hor1-48;
+                    minut1=((minut-48)*10)+minut1-48;     
+                    hora=hor1;minuta=minut1;
+                    Serial.println("sync:");Serial.print(hor1);Serial.print(":");Serial.print(minut1);
+                    biit='e';
+                    minut=-1;
+                    hor=-1;
+                    hor1=-1;
+                    minut1=-1;
+                    alarme=1;
+                    break; 
+                }
+              }
+            }
+          }
+        }
+      }        
+    }
+  } 
+    while (biit=='h'){
+      if (Serial.available()){
+        hor=Serial.read();
+        while (hor>-1){
+          if (Serial.available()){
+          hor1=Serial.read();
+            while (hor1>-1){
+              if (Serial.available()){
+                minut=Serial.read();
+                while (minut>-1){
+                  if (Serial.available()){
+                    minut1=Serial.read();
+                    hor1=((hor-48)*10)+hor1-48;
+                    minut1=((minut-48)*10)+minut1-48;
+                    
+                    setTime(hor1,minut1,0,1,1,2000);
+                    Serial.println("sync:");Serial.print(hor1);Serial.print(":");Serial.print(minut1);
+                    biit='e';
+                    minut=-1;
+                    hor=-1;
+                    hor1=-1;
+                    minut1=-1;
+                    break; 
+                }
+              }
+            }
+          }
+        }
+      }        
+    }
+  } 
+    if (biit=='l'){
+      digitalWrite(12,1);
+      Serial.println("Liga");
+      timer=0;desligar=timerr;
+      digitalWrite(3,0);
+      Status=1; 
+    }
+    
+    else if (biit=='d'){
+      digitalWrite(12,0);
+      Serial.println("desLiga");
+      timer=0;desligar=0;
+      digitalWrite(3,0);
+      Status=0; 
+    }
+    else if (biit=='@'){
+      if (Status2){
+        digitalWrite(13,0);
+        Serial.println("desLigaLCD");
+        Status2=0; b=0;
+      }
+      else if (!Status2){
+        digitalWrite(13,1);
+        Serial.println("LigaLCD");
+        Status2=1; b=0;
+      }
+    }  
+    else if (biit=='M'){
+      if (mudo){
+        mudo=0; b=0;
+      }
+      else if (!mudo){
+        mudo=1; b=0;
+      }
+    }
+    if(mudo!=1){digitalWrite(3,1);
+    delay(50);
+    digitalWrite(3,0);}
+  } 
+
+  
+  if (Rec.decode(&results) && b>100) {  //integrar mudo ao controle
+    Serial.println("infraVermelho:");
     if (results.value==0x800F840C && !Status){
       if (b>100){
         digitalWrite(12,1);
@@ -53,6 +184,7 @@ void loop() {
         Status=1;b=0;desligar=timerr;
       }  
     }
+    
     else if (results.value==0x800F840C && Status){
       if (b>100){
         digitalWrite(12,0);
@@ -60,6 +192,7 @@ void loop() {
         Status=0; b=0;desligar=0;
       }
     }
+    
     else if (results.value==0x800F040C && !Status){
       if (b>100){
         digitalWrite(12,1);
@@ -67,6 +200,7 @@ void loop() {
         Status=1;b=0;desligar=timerr;
       } 
     }
+    
     else if (results.value==0x800F040C && Status){
       if (b>100){
         digitalWrite(12,0);
@@ -74,6 +208,7 @@ void loop() {
         Status=0; b=0;desligar=0;
       }
     }
+    
     else if (results.value==0x800F840D || results.value==0x800F040D){
       if (Status2 && b>100){
         digitalWrite(13,0);
@@ -86,19 +221,22 @@ void loop() {
         Status2=1; b=0;
       }
     }
+    
     else if (results.value==0x800F0424 || results.value==0x800F8424){
       Rec.resume();
       while (parado<200){
-        lcd.setCursor(0,0);lcd.print("Editando Hora ti=");lcd.print(timerr);
-        synchora();
+        lcd.setCursor(0,0);lcd.print("Editando Hora ");lcd.print(timerr);
+        MostraTempo();
         if (Rec.decode(&results)){
           if (results.value==0x800F0480 || results.value==0x800F8480){
            parado=0;
              hor++;  
+             setTime(hor,minut,0,1,1,2000);
           }
           else if (results.value==0x800F0481 || results.value==0x800F8481){
               parado=0;
-              minut++;  
+              minut++; 
+              setTime(hor,minut,0,1,1,2000); 
           }
           else if (results.value==0x800F840D || results.value==0x800F040D){
             if (Status2 && b>100){
@@ -122,6 +260,8 @@ void loop() {
         parado++; 
       }
     }
+
+    
     else if (results.value==0x800F0480 || results.value==0x800F8480){
       Rec.resume();
       while (parado<200){
@@ -176,6 +316,9 @@ void loop() {
     parado=0;
     Rec.resume();
     b++;
+    if(mudo!=1){digitalWrite(3,1);
+    delay(50);
+    digitalWrite(3,0);}
     }
   if (digitalRead(4)==0){
     Serial.println("Botão:");
@@ -194,10 +337,10 @@ void loop() {
       Status=0; 
     }
     while(digitalRead(4)==0){
-      synchora();
+      MostraTempo();
       }    
   }
-  if (hora==hor && minut==minuta && alarme) {
+  if (hora==hour() && minute()==minuta && alarme) {
     timer=definido;
     alarme=0;
   }
@@ -207,36 +350,6 @@ void loop() {
     Status=0;
   }
 }
-void synchora(){
-  lcd.setCursor(0,1);lcd.print(hor);lcd.print(":");lcd.print(minut);lcd.print(':');lcd.print(seg);lcd.print(" ");lcd.print(temp);lcd.print("oC");if (alarme) {lcd.print(" a");} else if (desligar>0){lcd.print(desligar);} else {lcd.print("    ");}
-  if ((millis()-milise)>=1000){
-    milise=milise+1000;
-    seg++;    
-  }
-  if (seg>=60){
-    if(desligar>0)desligar--;
-    seg=0;
-    minut++;
-  }
-  if (minut>=60){
-    minut=0;
-    hor++;
-  }
-  if (hor>=24) hor=0;
-}
 void syncalarme(){
   lcd.setCursor(0,1);lcd.print(hora);lcd.print(":");lcd.print(minuta);lcd.print(" | ");lcd.print(definido);lcd.print("   ");
-    if ((millis()-milise)>1000){
-    milise=milise+1000;
-    seg++;    
-  }
-  if (seg>=60){
-    seg=0;
-    minut++;
-  }
-  if (minut>=60){
-    minut=0;
-    hor++;
-  }
-  if (hor>=24) hor=0;
 }
